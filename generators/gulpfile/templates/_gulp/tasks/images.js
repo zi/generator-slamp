a596ab156aee736685608871d258db3d148d5247
Marco Ziliani <marco.ziliani@brand039.com>
2015-11-06 11:12:09 +0100

    gulpfile: fix bug in images task

--- generators/gulpfile/templates/_gulp/tasks/images.js
+++ generators/gulpfile/templates/_gulp/tasks/images.js
@@ -7,15 +7,15 @@ var config = require('../config');
 
 gulp.task('images', function() {
   return gulp.src(config.files.images)
-    .pipe($.debug({title: 'debug: images'}))
-    .pipe($.if($.if.isFile, $.cache($.imagemin({
+    .pipe($.imagemin({
       progressive: true,
       interlaced: true,
       svgoPlugins: [{cleanupIDs: false}] // don't remove IDs from SVGs, they are often used as hooks for embedding and styling
     }))
+    .pipe($.debug({title: 'debug: images'}))
     .on('error', function(err) {
       console.log(err);
       this.end();
-    })))
+    })
     .pipe(gulp.dest(config.srcDir + '/images'));
 });
